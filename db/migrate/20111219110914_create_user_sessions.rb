f9e8a4e60acbc8f1c542abdb8cce90b448e904c1
Allon Hadaya <allonhadaya@hotmail.com>
2011-12-19 06:13:34 -0500

    user session model and migration edited

--- db/migrate/20111219110914_create_user_sessions.rb
+++ db/migrate/20111219110914_create_user_sessions.rb
@@ -1,8 +1,15 @@
 class CreateUserSessions < ActiveRecord::Migration
-  def change
-    create_table :user_sessions do |t|
+	def self.up
+		create_table :sessions do |t|
+			t.string :session_id, :null => false
+			t.text :data
+			t.timestamps
+		end
+		add_index :sessions, :session_id
+		add_index :sessions, :updated_at
+	end
 
-      t.timestamps
-    end
-  end
+	def self.down
+		drop_table :sessions
+	end
 end
