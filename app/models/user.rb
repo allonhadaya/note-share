677b4e9dc37af491ee05cc9184a561de964c8b12
Allon Hadaya <allonhadaya@hotmail.com>
2012-01-01 21:41:15 -0500

    stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	acts_as_authentic
+	has_many :groups # admin
+	has_and_belongs_to_many :groups # member
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 NoteShare::Application.routes.draw do
 	resources :user_sessions
-	match 'login' => "user_sessions#new", :as => :login
-	match 'logout' => "user_sessions#destroy", :as => :logout
+	resources :users
+	resource :user, :as => 'account'
 	
-	resources :users  # give us our some normal resource routes for users
-	resource :user, :as => 'account'  # a convenience route
 	match 'signup' => 'users#new', :as => :signup
+	match 'login' => 'user_sessions#new', :as => :login
+	match 'logout' => 'user_sessions#destroy', :as => :logout
+	
+	match ':group_id' => 'notes#group'
+	match ':group_id/settings' => 'notes#group_settings'
+	match ':group_id/pad/:pad_id' => 'notes#pad'
 end
